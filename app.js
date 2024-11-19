19938267e09cf79786469633dd5f7452e3fecc77
MrDanPixel <mr.jony.nice@gmail.com>
2024-11-19 11:36:32 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -2,29 +2,61 @@ let tg = window.Telegram.WebApp;
 tg.expand();
 tg.MainButton.textColor = "#FFFFFF";
 tg.MainButton.color = "#2cab37";
+
 let item = "";
 let btn1 = document.getElementById("btn1");
 let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+
 btn1.addEventListener("click", function () {
-if (tg.MainButton.isVisible) {
-tg.MainButton.hide();
-}
-else {
-tg.MainButton.setText("Вывести информацию по овену");
-item = "1";
-tg.MainButton.show();
-}
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по жанру Футбол");
+        item = "1";
+        tg.MainButton.show();
+    }
 });
+
 btn2.addEventListener("click", function () {
-if (tg.MainButton.isVisible) {
-tg.MainButton.hide();
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по жанру Баскетбол");
+        item = "2";
+        tg.MainButton.show();
+    }
+});
+
+btn3.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по жанру Волейбол");
+        item = "3";
+        tg.MainButton.show();
+    }
+});
+
+btn4.addEventListener("click", function () {
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по жанру Настольный теннис");
+        item = "4";
+        tg.MainButton.show();
+    }
+});
+
+function sendRecipeData(recipeId) {
+    Telegram.WebApp.sendData(JSON.stringify({ item_id: recipeId }));
 }
-else {
-tg.MainButton.setText("Вывести информацию по тельцу");
-item = "2";
-tg.MainButton.show();
-}
-55});
+
 Telegram.WebApp.onEvent("mainButtonClicked", function () {
-tg.sendData(item);
+    tg.sendData(item);
 });
