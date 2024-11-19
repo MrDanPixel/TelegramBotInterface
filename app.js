f8b635612daf378d9f00e7f4cc83b9b950e61335
MrDanPixel <mr.jony.nice@gmail.com>
2024-11-19 15:45:17 +0400

    Update app.js

--- app.js
+++ app.js
@@ -9,40 +9,54 @@ let btn2 = document.getElementById("btn2");
 let btn3 = document.getElementById("btn3");
 let btn4 = document.getElementById("btn4");
 
-function fetchData(itemId) {
-    fetch('https://your-server-url/get_data', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ item_id: itemId })
-    })
-    .then(response => response.json())
-    .then(data => {
-        // Обработка полученных данных
-        console.log(data);
-        // Вывод данных на страницу
-        document.getElementById('usercard').innerText = JSON.stringify(data);
-    })
-    .catch(error => console.error('Error:', error));
-}
-
 btn1.addEventListener("click", function () {
-    item = "1";
-    fetchData(item);
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по жанру Футбол");
+        item = "1";
+        tg.MainButton.show();
+    }
 });
 
 btn2.addEventListener("click", function () {
-    item = "2";
-    fetchData(item);
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по жанру Баскетбол");
+        item = "2";
+        tg.MainButton.show();
+    }
 });
 
 btn3.addEventListener("click", function () {
-    item = "3";
-    fetchData(item);
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по жанру Волейбол");
+        item = "3";
+        tg.MainButton.show();
+    }
 });
 
 btn4.addEventListener("click", function () {
-    item = "4";
-    fetchData(item);
+    if (tg.MainButton.isVisible) {
+        tg.MainButton.hide();
+    }
+    else {
+        tg.MainButton.setText("Вывести информацию по жанру Настольный теннис");
+        item = "4";
+        tg.MainButton.show();
+    }
+});
+
+function sendRecipeData(recipeId) {
+    Telegram.WebApp.sendData(JSON.stringify({ item_id: recipeId }));
+}
+
+Telegram.WebApp.onEvent("mainButtonClicked", function () {
+    tg.sendData(item);
 });
